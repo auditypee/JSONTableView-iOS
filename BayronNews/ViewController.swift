0613a4bad37049406b9d3a33f0b637b95673216a
Audi Bayron <z1789895@doits-mac-mini-6.local>
2017-04-05 09:16:08 -0500

    Initial Commit

---
+++ BayronNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BayronNews
+//
+//  Created by Audi Bayron on 4/5/17.
+//  Copyright © 2017 Audi Bayron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
